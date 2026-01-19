edb937af02858dfc9a2ca40d54b03b88f495dfe4
Aditya-Kotame02 <adityakotame17@gmail.com>
2026-01-19 21:16:08 +0530

    added base React app structure

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { createContext, useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Unauthorized from './pages/Unauthorized';
+import RoleRoute from './components/RoleRoute';
+import Signin from './pages/signin';
+import Signup from './pages/signup';
+import { ToastContainer } from 'react-toastify';
+import RetailerHome from './pages/Retailer/RetailerHome';
+import AdminHome from './pages/Admin/AdminHome';
+import WholesalerHome from './pages/WholeSaler/WholeSalerHome';
+
+export const UserContext = createContext()
+function App() {
+  const [user, setUser] = useState(null)
+
+  return (
+    <>
+      <UserContext.Provider value={{user, setUser}} >
+        <Routes>
+          <Route path='*' element={<Signin />}/>
+          <Route path='/register' element={<Signup />} />
+          <Route path='/unauthorized' element={<Unauthorized />} />
+
+          <Route path='/retailer'
+            element={
+              <RoleRoute allowedRoles={['RETAILER']}>
+                <RetailerHome />
+              </RoleRoute>
+            }>
+
+            {/* <Route path='profile' /> */}
+          </Route>
+
+          <Route
+            path='/admin'
+            element={
+              <RoleRoute allowedRoles={['ADMIN']}>
+                <AdminHome />
+              </RoleRoute>
+            }
+          >
+            {/* <Route path='addItems'/> */}
+          </Route>
+
+          <Route
+            path='/wholesaler'
+            element={
+              <RoleRoute allowedRoles={['WHOLESALER']}>
+                <WholesalerHome />
+              </RoleRoute>
+            }
+          />
+       
+        </Routes>
+      </UserContext.Provider>
+      <ToastContainer />
+    </>
+  )
+}
+
+export default App
