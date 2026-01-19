d9bb5a0ee2b6e4470e24389ff3b275b933174f13
Abhishek <abhishekpagar2020@gmail.com>
2026-01-19 23:59:54 +0530

    retailer page start

--- src/pages/Retailer/RetailerHome.jsx
+++ src/pages/Retailer/RetailerHome.jsx
@@ -1,4 +1,10 @@
 const RetailerHome = () => {
-  return <h2>Retailer Dashboard</h2>
+  return (
+    <div>
+      <h2>Retailer Dashboard</h2>
+      <h1>This is Retailer Home Page</h1>
+    </div>
+  )
+
 }
 export default RetailerHome
